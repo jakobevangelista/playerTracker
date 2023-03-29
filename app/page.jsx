4abf4545cc449086b53fd821b5a2167078dd5f59
Jakob Evangelista <jakobevangelista@tamu.edu>
2023-03-29 17:25:47 -0500

    messing around with login

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,22 @@ import { useEffect, useState } from "react";
 
 const pb = new PocketBase("http://45.33.6.9:81");
 
-export default function Home() {
+async function getTables() {
+  // const records = await pb.collection("table").getFullList({
+  //   sort: "-created",
+  // });
+  const res = await fetch(
+    "http://45.33.6.9:81/_/#/collections?collectionId=6hefqmhdmxwit5e&filter=&sort=-created"
+  );
+  data = await res.json();
+  console.log(data);
+  return data;
+}
+
+export default async function Home() {
   const router = useRouter();
   const [username, setUsername] = useState("");
+  // const [records, setRecords] = useState("");
   // const [userInput, setUserInput] = useState("");
 
   useEffect(() => {
@@ -16,22 +29,29 @@ export default function Home() {
     } else {
       setUsername(pb.authStore.model.username);
     }
-
-    // you can also fetch all records at once via getFullList
-    const records = pb.collection("table").getFullList({
-      sort: "-created",
-    });
   });
 
   function logout() {
     pb.authStore.clear();
     router.push("/login");
   }
+  // const tables = await getTables();
 
   return (
     <>
-      <h1>We're back {username}</h1>
+      <h1>We back {username}</h1>
       <button onClick={logout}>Logout</button>
+      {/* <div>
+        {tables.map((table) => {
+          return <Table key={table.id} />;
+        })}
+      </div> */}
     </>
   );
 }
+
+// function Table(table) {
+//   const { id } = table;
+
+//   return <div>{id}</div>;
+// }
