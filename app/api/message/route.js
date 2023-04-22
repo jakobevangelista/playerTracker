d989a87408ab034193e46af7f821927ae9d5e835
Jakob Evangelista <jakobevangelista@tamu.edu>
2023-04-21 21:30:29 -0500

    fixed async function in API handler

--- app/api/message/route.js
+++ app/api/message/route.js
@@ -6,7 +6,7 @@ export async function POST(request) {
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = twilio(accountSid, authToken);
-  client.messages
+  await client.messages
     .create({
       body: body.message,
       from: "+19798145647",
